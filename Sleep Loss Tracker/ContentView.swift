ff884ad8c987488d84424c501a3f26dea178828b
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-08 20:01:54 -0600

    Initial Commit

---
+++ Sleep Loss Tracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sleep Loss Tracker
+//
+//  Created by Alex Strugacz on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sleep Loss Tracker/Sleep_Loss_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_Loss_TrackerApp.swift
+//  Sleep Loss Tracker
+//
+//  Created by Alex Strugacz on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_Loss_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
