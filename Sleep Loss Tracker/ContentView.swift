a959daedf8634023b787caec22f40e1517a98416
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-09 23:14:24 -0600

    basic UI, initial commit

--- Sleep Loss Tracker/ContentView.swift
+++ Sleep Loss Tracker/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            
+            VStack {
+            
+                Text("Sleep Loss Tracker")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding([.top, .trailing], 50.0)
+                    
+                HStack {
+                    Spacer()
+                    Image(systemName: "powersleep")
+                        .resizable()
+                        .foregroundColor(Color(hue: 0.75, saturation: 0.563, brightness: 0.709))
+                        .frame(width: 30, height: 30)
+                    
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Spacer()
+                }
+                
+                .padding()
+                Spacer()
+                Text("Hello, world!")
+                
+            }
         }
-        .padding()
     }
 }
 
